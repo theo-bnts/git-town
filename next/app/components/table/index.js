a1f7c0926e6815c8ab889288a3207eb28318bb9d
Maël RHUIN <66364083+LIncorruptible@users.noreply.github.com>
2025-02-04 10:10:38 +0100

    Arborescence composant Table

---
+++ next/app/components/table/index.js
@@ -0,0 +1,6 @@
+export { default as Table } from './Table';
+export { default as TableHeader } from './TableHeader';
+export { default as TableRow } from './TableRow';
+export { default as TableCell } from './TableCell';
+export { default as SortButton } from './SortButton';
+export { default as ActionButton } from './ActionButton';
