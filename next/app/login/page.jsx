b3a0570813244fac6fd67408321657d4fc4861cf
Dorian Descamps <dorian.descamps601@gmail.com>
2025-02-20 23:57:49 +0100

    auth feature

--- next/app/login/page.jsx
+++ next/app/login/page.jsx
@@ -11,18 +11,15 @@ import miageLogo from '../../public/assets/pictures/miage.png';
 export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Logo en haut SEULEMENT sur mobile */}
       <div className="md:hidden flex justify-center pt-2">
         <Image src={gittownlogo} alt="GitTown logo" width={300} height={200} />
       </div>
 
       <div className="flex-1 flex flex-col md:flex-row items-center justify-center max-w-screen-2xl mx-auto w-full px-4">
-        {/* Logo Gittown affiché uniquement sur md+ */}
         <div className="hidden md:flex flex-1 items-center justify-center">
           <Image src={gittownlogo} alt="GitTown logo" width={400} height={400} />
         </div>
 
-        {/* Zone de formulaire */}
         <div className="flex-1 flex items-center justify-center w-full">
           <div className="w-full sm:max-w-md">
             <ManageAuthForm />
@@ -30,7 +27,6 @@ export default function LoginPage() {
         </div>
       </div>
 
-      {/* Logo Miage en bas */}
       <div className="flex justify-center pb-2">
         <Image src={miageLogo} alt="Miage logo" width={300} height={300} />
       </div>
