096ce7ac4a25b29c07f91c0bc4f14abdb8c12cf2
LIncorrutpible <maelrhuin@gmail.com>
2025-03-15 18:34:29 +0000

    Ajout scroll dans ListBoxArea

--- next/app/components/ui/listbox/ListBoxArea.jsx
+++ next/app/components/ui/listbox/ListBoxArea.jsx
@@ -4,7 +4,7 @@ import Card from '@/app/components/ui/Card';
 
 export default function ListBoxArea({ items, onRemove }) {
     return (
-        <div className="flex flex-col bg-white border border-gray-200 rounded-[12.5px]">
+        <div className="flex flex-col bg-white border border-gray-200 rounded-[12.5px] max-h-[160px] overflow-y-auto">
             {items.length === 0 ? (
                 <Card variant="empty_list">
                     <p className="text-center text-gray-600">Aucun élément sélectionné.</p>
