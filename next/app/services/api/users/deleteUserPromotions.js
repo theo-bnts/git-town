9dae9a0403f4c067c781b2191943ed74a7290283
LIncorruptible <maelrhuin@gmail.com>
2025-03-29 09:54:05 +0000

    Correction erreur de suppression des liens entre un user et ses promotions

--- next/app/services/api/users/deleteUserPromotions.js
+++ next/app/services/api/users/deleteUserPromotions.js
@@ -1,43 +1,30 @@
-import { userPromotionsRoute, deleteUserPromotionRoute } from "@/app/services/routes";
+import { deleteUserPromotionRoute } from "@/app/services/routes";
+import getUserPromotions from "@/app/services/api/users/getUserPromotions";
 import { handleApiError } from "@/app/services/errorHandler";
 
 /**
- * Supprime les liens entre un utilisateur et ses promotions
- * (DELETE /users/:userId/promotions)
+ * Supprime tous les liens entre un utilisateur et ses promotions
+ * (DELETE /users/:userId/promotions/:promotionId)
  *
  * @param {string} userId - L'identifiant de l'utilisateur.
  * @param {string} token - Le token d'authentification.
- * @returns {Promise<void>}
- * @throws {Error} - En cas d'erreur lors de la suppression.
+ * @returns {Promise<void>} 
+ * @throws {Error} - En cas d'erreur lors de la suppression d'un lien.
  */
 export default async function deleteUserPromotions(userId, token) {
-  const getUrl = userPromotionsRoute(userId);
-  let res = await fetch(getUrl, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  let text = await res.text();
-  let promotions = text ? JSON.parse(text) : [];
-  
-  if (!res.ok) {
-    throw handleApiError(res, promotions);
-  }
+  const currentPromotions = await getUserPromotions(userId, token);
   
   await Promise.all(
-    promotions.map(async (promotion) => {
-      const deleteUrl = deleteUserPromotionRoute(userId, promotion.Id);
-      const delRes = await fetch(deleteUrl, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+    currentPromotions.map(async (promotionLink) => {
+      const promotionId = promotionLink.Promotion.Id;
+      const endpoint = deleteUserPromotionRoute(userId, promotionId);
+      const res = await fetch(endpoint, {
+        method: 'DELETE',
+        headers: { Authorization: `Bearer ${token}` }
       });
-      const delText = await delRes.text();
-      const delData = delText ? JSON.parse(delText) : {};
-      if (!delRes.ok) {
-        throw handleApiError(delRes, delData);
+      if (!res.ok) {
+        const data = await res.json();
+        throw handleApiError(res, data);
       }
     })
   );
