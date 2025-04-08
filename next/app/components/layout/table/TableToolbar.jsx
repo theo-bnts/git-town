4baad98e533c38a81c75fb227e518dc0974dc3c1
Dorian Descamps <dorian.descamps601@gmail.com>
2025-04-08 20:59:58 +0000

    toolbar padding

--- next/app/components/layout/table/TableToolbar.jsx
+++ next/app/components/layout/table/TableToolbar.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function TableToolbar({ children }) {
   return (
-    <div className="flex items-center justify-start">
+    <div className="flex items-center justify-start pb-8">
       <div className="space-x-4">
         {children}
       </div>
