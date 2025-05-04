a0437536135a3a88291a3e7bb3ce735c39a47139
LIncorruptible <maelrhuin@gmail.com>
2025-05-04 13:22:27 +0000

    Suppression de lignes vides dans InfoTooltip, LanguagesSection et LoadingCard

--- next/app/components/ui/modal/statistics/InfoTooltip.jsx
+++ next/app/components/ui/modal/statistics/InfoTooltip.jsx
@@ -1,9 +1,7 @@
 'use client';
 
 import React from 'react';
-
 import Card from '@/app/components/ui/Card';
-
 import { textStyles } from '@/app/styles/tailwindStyles';
 
 /**
--- next/app/components/ui/modal/statistics/LanguagesSection.jsx
+++ next/app/components/ui/modal/statistics/LanguagesSection.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-
 import { textStyles } from '@/app/styles/tailwindStyles';
 
 /**
